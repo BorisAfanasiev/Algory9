148cc94f4d997c2c58ef69a2ae8ed136c1703ab3
Борис Афанасьев <bobbolkov@gmail.com>
2021-12-24 14:35:11 +0200

    Implemented new Heron algorithm for area of triangle.

--- Algory9/main.cpp
+++ Algory9/main.cpp
@@ -6,7 +6,7 @@ int main()
 	Triangle one{ Point(2,3), Point(1,1), Point(-3,4) };		//area - 5.5
 	Triangle two{ Point(2,3), Point(1,2), Point(-3,4) };		//area - 3
 
-	std::cout << two.getPerimeter();
+	std::cout << one.getArea();
 	std::cout << std::endl;
-	std::cout << two.getSemiPerimetr();
+	std::cout << two.getArea();
 }
